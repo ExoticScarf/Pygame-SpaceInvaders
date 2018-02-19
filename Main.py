d16e0517a578b46ae2195933e1a7069ed0571348
Adam Clarke <adam.james.clarke1@gmail.com>
2018-02-19 00:48:00 +0000

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
+import pygame
 import random
 import time
-
-from Objs import *
 
 DisplayWidth = 800
 DisplayHeight = 600
@@ -10,6 +9,96 @@ pygame.init()
 GameDisplay = pygame.display.set_mode((DisplayWidth, DisplayHeight))
 pygame.display.set_caption("Space Invaders")
 Clock = pygame.time.Clock()
+
+SmallTextSize = 40
+SmallText = pygame.font.Font("PressStart2P-Regular.ttf", SmallTextSize)
+
+LargeTextSize = 100
+LargeText = pygame.font.Font("PressStart2P-Regular.ttf", LargeTextSize)
+
+Black = (0, 0, 0)
+White = (255, 255, 255)
+Green = (0, 255, 0)
+
+FPS = 120
+
+Tier1_1 = pygame.image.load("Sprites\\Enemies\\Tier1Invader_Instance1.png")
+Tier1_2 = pygame.image.load("Sprites\\Enemies\\Tier1Invader_Instance2.png")
+Tier2_1 = pygame.image.load("Sprites\\Enemies\\Tier2Invader_Instance1.png")
+Tier2_2 = pygame.image.load("Sprites\\Enemies\\Tier2Invader_Instance2.png")
+Tier3_1 = pygame.image.load("Sprites\\Enemies\\Tier3Invader_Instance1.png")
+Tier3_2 = pygame.image.load("Sprites\\Enemies\\Tier3Invader_Instance2.png")
+MotherShip = pygame.image.load("Sprites\\Enemies\\MotherShip.png")
+
+Player = pygame.image.load("Sprites\\Player\\Player.png")
+PlayerFire = pygame.image.load("Sprites\\Player\\PlayerFire.png")
+
+ShieldBlockHP4 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBlockHP4.png")
+ShieldBlockHP3 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBlockHP3.png")
+ShieldBlockHP2 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBlockHP2.png")
+ShieldBlockHP1 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBlockHP1.png")
+
+ShieldTopLeftHP4 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopLeftHP4.png")
+ShieldTopLeftHP3 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopLeftHP3.png")
+ShieldTopLeftHP2 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopLeftHP2.png")
+ShieldTopLeftHP1 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopLeftHP1.png")
+
+ShieldTopRightHP4 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopRightHP4.png")
+ShieldTopRightHP3 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopRightHP3.png")
+ShieldTopRightHP2 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopRightHP2.png")
+ShieldTopRightHP1 = pygame.image.load("Sprites\\ShieldPieces\\ShieldTopRightHP1.png")
+
+ShieldBottomLeftHP4 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomLeftHP4.png")
+ShieldBottomLeftHP3 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomLeftHP3.png")
+ShieldBottomLeftHP2 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomLeftHP2.png")
+ShieldBottomLeftHP1 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomLeftHP1.png")
+
+ShieldBottomRightHP4 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomRightHP4.png")
+ShieldBottomRightHP3 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomRightHP3.png")
+ShieldBottomRightHP2 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomRightHP2.png")
+ShieldBottomRightHP1 = pygame.image.load("Sprites\\ShieldPieces\\ShieldBottomRightHP1.png")
+
+PlayerFireSound = pygame.mixer.Sound("Sound\\PlayerFireSound.wav")
+InvaderFireSound = pygame.mixer.Sound("Sound\\InvaderFireSound.wav")
+MotherShipSound = pygame.mixer.Sound("Sound\\MotherShipSound.wav")
+InvaderKilledSound = pygame.mixer.Sound("Sound\\InvaderKilledSound.wav")
+PlayerDeathSound = pygame.mixer.Sound("Sound\\PlayerDeathSound.wav")
+
+BackgroundSound1 = pygame.mixer.Sound("Sound\\BackgroundSound1.wav")
+BackgroundSound2 = pygame.mixer.Sound("Sound\\BackgroundSound2.wav")
+BackgroundSound3 = pygame.mixer.Sound("Sound\\BackgroundSound3.wav")
+BackgroundSound4 = pygame.mixer.Sound("Sound\\BackgroundSound4.wav")
+
+Tier1Width = 24
+Tier1Height = 18
+
+Tier2Width = 22
+Tier2Height = 16
+
+Tier3Width = 16
+Tier3Height = 16
+
+MotherShipWidth = 36
+MotherShipHeight = 14
+
+ShieldBlockWidth = 16
+ShieldBlockHeight = 16
+ShieldTopWidth = 14
+ShieldTopHeight = 14
+ShieldBottomWidth = 12
+SheildBottomHeight = 12
+
+PlayerWidth = 50
+PlayerHeight = 26
+PlayerFireWidth = 2
+PlayerFireHeight = 20
+
+ButtonWidth = 200
+ButtonHeight = SmallTextSize + 10
+
+InvaderRows = 5
+InvaderNum = 100
+RowCount = InvaderNum / InvaderRows
 
 Paused = False
 
@@ -35,14 +124,15 @@ def ButtonGen(Text, Font, ButtonX, ButtonY, InactiveColour, ActiveColour, Functi
     GameDisplay.blit(TextSurf, TextRect)
 
 
+def ShieldGen():
+    pass
+
+
 def InvaderGen():
     pass
 
+
 def EnemyFireGen():
-    pass
-
-
-def PlayerFireGen():
     pass
 
 
@@ -75,7 +165,7 @@ def PauseScreen():
 
         time.sleep(0.1)
 
-        Clock.tick(60)
+        Clock.tick(FPS)
 
 
 def UnPause():
@@ -103,13 +193,63 @@ def MainMenu():
 
         ButtonGen("Play", SmallText, DisplayWidth / 2 - ButtonWidth / 2, DisplayHeight / 2, White, Green, GameLoop)
         pygame.display.update()
-        Clock.tick(60)
+        Clock.tick(FPS)
 
 
 def GameLoop():
     PlayerX = DisplayWidth / 2 - PlayerWidth / 2
     PlayerY = DisplayHeight - PlayerHeight - 10
     PlayerMovePoints = 0
+
+    PlayerFireX = -100
+    PlayerFireY = -100
+
+    InvaderX = []
+    InvaderY = []
+    Invaders = [InvaderX, InvaderY]
+
+    Type = 0
+    SoundType = 0
+    InvaderMoveType = 0
+    InvaderDir = "Right"
+    InvaderSpeed = 5
+
+    for i in range(InvaderNum):
+
+        if 0 <= i < 20:
+            InvaderY.append(40)
+            if i == 0:
+                InvaderX.append(100)
+            else:
+                InvaderX.append(InvaderX[i - 1] + 30)
+
+        elif 20 <= i < 40:
+            InvaderY.append(80)
+            if i == 20:
+                InvaderX.append(100)
+            else:
+                InvaderX.append(InvaderX[i - 1] + 30)
+
+        elif 40 <= i < 60:
+            InvaderY.append(120)
+            if i == 40:
+                InvaderX.append(100)
+            else:
+                InvaderX.append(InvaderX[i - 1] + 30)
+
+        elif 60 <= i < 80:
+            InvaderY.append(160)
+            if i == 60:
+                InvaderX.append(100)
+            else:
+                InvaderX.append(InvaderX[i - 1] + 30)
+
+        elif 80 <= i <= 100:
+            InvaderY.append(200)
+            if i == 80:
+                InvaderX.append(100)
+            else:
+                InvaderX.append(InvaderX[i - 1] + 30)
 
     global Paused
 
@@ -129,7 +269,9 @@ def GameLoop():
                     PlayerMovePoints = 5
 
                 if event.key == pygame.K_SPACE or event.key == pygame.K_w or event.key == pygame.K_UP:
-                    PlayerFireGen()
+                    if PlayerFireY == -100 and PlayerFireX == -100:
+                        PlayerFireX = PlayerX + PlayerWidth / 2 - 1
+                        PlayerFireY = PlayerY + 2 - PlayerFireHeight
 
                 if event.key == pygame.K_p:
                     Paused = True
@@ -149,8 +291,60 @@ def GameLoop():
         GameDisplay.fill(Black)
         GameDisplay.blit(Player, (PlayerX, PlayerY))
 
+        GameDisplay.blit(PlayerFire, (PlayerFireX, PlayerFireY))
+
+        for i in range(InvaderNum):
+            if i < RowCount and Type < FPS:
+                GameDisplay.blit(Tier3_1, (Invaders[0][i], Invaders[1][i]))
+            elif i < RowCount and Type < FPS * 2:
+                GameDisplay.blit(Tier3_2, (Invaders[0][i], Invaders[1][i]))
+
+            elif i < RowCount * 3 and Type < FPS:
+                GameDisplay.blit(Tier2_1, (Invaders[0][i], Invaders[1][i]))
+            elif i < RowCount * 3 and Type < FPS * 2:
+                GameDisplay.blit(Tier2_2, (Invaders[0][i], Invaders[1][i]))
+
+            elif i < InvaderNum and Type < FPS:
+                GameDisplay.blit(Tier1_1, (Invaders[0][i], Invaders[1][i]))
+            elif i < InvaderNum and Type < FPS * 2:
+                GameDisplay.blit(Tier1_2, (Invaders[0][i], Invaders[1][i]))
+
+        InvaderMoveType += 1
+        if InvaderMoveType > FPS:
+            InvaderMoveType = 0
+
+            for i in range(len(Invaders[0])):
+                if InvaderDir == "Right":
+                    if Invaders[0][i] < DisplayWidth - Tier3Width - 20:
+                        Invaders[0][i] += InvaderSpeed
+                    else:
+                        InvaderDir = "Left"
+
+                elif InvaderDir == "Left":
+                    if Invaders[0][i] > 10:
+                        Invaders[0][i] -= InvaderSpeed
+                    else:
+                        InvaderDir = "Right"
+
+        Type += 1
+        if Type > FPS * 2:
+            Type = 0
+
+        SoundType += 1
+        if SoundType > FPS * 4:
+            SoundType = 0
+
+        elif SoundType == FPS:
+            pygame.mixer.Sound.play(BackgroundSound4)
+        elif SoundType == FPS * 2:
+            pygame.mixer.Sound.play(BackgroundSound2)
+        elif SoundType == FPS * 3:
+            pygame.mixer.Sound.play(BackgroundSound1)
+        elif SoundType == FPS * 4:
+            pygame.mixer.Sound.play(BackgroundSound3)
+
         pygame.display.update()
-        Clock.tick(60)
+        Clock.tick(FPS)
 
 
 MainMenu()
